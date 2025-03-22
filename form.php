5616021467f54bedfa3ffd3fec10600353984988
Bill-git0119 <zhangbaixun93@gmail.com>
2025-03-22 20:32:27 +0800

    Add files via upload

--- form.php
+++ form.php
@@ -1,51 +1,46 @@
+<?php
+session_start();
+?>
+
 <html>
 <head></head>
-
 <body bgcolor="#E8FFF5">
+<?php
+if (isset($_SESSION["check"])){
+    echo "welcome!";
+    echo "<a href='logout.php'>Logout</a>";
+    echo "<form action='info.php' method='post'>";
+    echo "<font size='4'>";
+    echo "please input your name:<input type='text' name='uName'><br>";
+    echo "please input your password:<input type='password' name='uPwd'><br>";
+    echo "please input your email:<input type='email' name='uemail'><br>";
+    echo "please select your gender:<input type='radio' name='ugender' value='male'>male<input type='radio' name='gender' value='female'>female<br>";
+    echo "please select your age:<input type='number' name='uage' min='18' max='60'><br>";
+    echo "please select your birthday:<input type'date' name='ubirth'><br>";
+    echo "please select how you like this webpage:<input type='range' name='ulike'><br>";
+    echo "<input type='hidden' name='usecret' value='only i can see'>";
 
-<form  action="info.php" method="post">
+    echo "please select your interest:";
+    echo "<input type='checkbox' name='uinterest[]' value='study'>study";
+    echo "<input type='checkbox' name='uinterest[]' value='sleep'>sleep";
+    echo "<input type='checkbox' name='uinterest[]' value='game'>game<br>";
 
-<font size="4">
-please input your name:<input type="text" name="uName" required><br>
-please input your password:<input type="password" name="uPwd" required><br>
-please input your email:<input type="email" name="uemail" required><br>
-please select your gender:<input type="radio" name="ugender" value="male">male<input type="radio" name="gender" value="female">female<br>
-please select your age:<input type="number" name="uage" min="18" max="60"><br>
-please select your birthday:<input type="date" name="ubirth"><br>
-please select how you like this webpage:<input type="range" name="ulike"><br>
-<input type="hidden" name="usecret" value="only i can see">
+    echo "please input your comments:";
+    echo "<br>";
+    echo "<textarea cols='30' rows='30' name='ucomment'></textarea>";
+    echo "</font>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br><input type='submit'>";
+    echo "<input type='reset'>";
+    echo "</form>";
+}else{
+    echo "illegal user!";
+    header("Refresh:2;url='login.php'");
+}
 
-please select your interest:
-<input type="checkbox" name="uinterest[]" value="study">study
-<input type="checkbox" name="uinterest[]" value="sleep">sleep
-<input type="checkbox" name="uinterest[]" value="game">game<br>
-
-please input your comments:
-<br>
-<textarea cols="30" rows="30" name="ucomment"></textarea>
-</font>
-<br>
-<br>
-<br>
-
-
-
-<br><input type="submit"><input type="reset">
-</form>
-
-
-
-
+    
+?>
 </body>
-
-
-
-
-
-
-
-
-
-
-
 </html>
--- info.php
+++ info.php
@@ -1,28 +1,28 @@
-
-
 <?php
-
-$uName = $_POST["uName"];
-$uPwd = $_POST["uPwd"];
-$uemail = $_POST["uemail"];
-$ugender=$_POST["ugender"];
-$uage=$_POST["uage"];
-$ubirth=$_POST["ubirth"];
-$ulike=$_POST["ulike"];
-$interest = isset($_POST["uinterest"]) ? $_POST["uinterest"] :[];
-$ucomment=$_POST["ucomment"];
-
-
-echo "your name is:".$uName."<br>";
-echo "your password is:".$uPwd."<br>";
-echo "your email is:".$uemail."<br>";
-echo "your gender is:".$ugender."<br>";
-echo "your age is:".$uage."<br>";
-echo "your birth is:".$ubirth."<br>";
-echo "your satisfication is:".$ulike."<br>";
-echo "your interest is: " . (!empty($interest) ? implode(", ", $interest) : "None") . "<br>";
+session_start();
+if(isset($_SESSION["check"])){
+    $uName = $_POST["uName"];
+    $uPwd = $_POST["uPwd"];
+    $uemail = $_POST["uemail"];
+    $ugender=$_POST["ugender"];
+    $uage=$_POST["uage"];
+    $ubirth=$_POST["ubirth"];
+    $ulike=$_POST["ulike"];
+    $interest = isset($_POST["uinterest"]) ? $_POST["uinterest"] :[];
+    $ucomment=$_POST["ucomment"];
 
 
-echo "your comment".nl2br(htmlentities($ucomment));
-
+    echo "your name is:".$uName."<br>";
+    echo "your password is:".$uPwd."<br>";
+    echo "your email is:".$uemail."<br>";
+    echo "your gender is:".$ugender."<br>";
+    echo "your age is:".$uage."<br>";
+    echo "your birth is:".$ubirth."<br>";
+    echo "your satisfication is:".$ulike."<br>";
+    echo "your interest is: " . (!empty($interest) ? implode(", ", $interest) : "None") . "<br>";
+    echo "your comment".nl2br(htmlentities($ucomment));
+}else{
+    echo"illegal user!";
+    header("Refresh:2;url='login.php'");
+}
 ?>
